dbfc3e520044663a6846a66c59f8581e7805ea7e
zhangliugang <www.zhangyi508@gmail.com>
2016-10-14 22:06:10 +0800

    change pay sign

--- src/components/WechatConect/WechatKit.jsx
+++ src/components/WechatConect/WechatKit.jsx
@@ -75,11 +75,11 @@ module.exports = {
               .end( (err, res) => {
                 if (res.ok) {
                   console.log(openid)
-                  console.log(res.body.nonceStr.xml.prepay_id)
+                  console.log(res.body.nonceStr.xml.nonceStr)
                   console.log(res.body.nonceStr)
                   wx.chooseWXPay({
                     timestamp: res.body.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                    nonceStr: res.body.nonceStr, // 支付签名随机串，不长于 32 位
+                    nonceStr: res.body.nonceStr.xml.nonce_str, // 支付签名随机串，不长于 32 位
                     package: 'prepay_id='+res.body.nonceStr.xml.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                     signType: res.body.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                     paySign: res.body.paySign, // 支付签名
