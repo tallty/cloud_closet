0f4894bcc0adc6c4ba72d53a77c86017fef725ca
iboying <weboying@gmail.com>
2016-10-20 16:52:20 +0800

    complete recharge

--- src/components/user/Recharge.jsx
+++ src/components/user/Recharge.jsx
@@ -6,44 +6,95 @@ import React, { Component, PropTypes } from 'react'
 import WechatKit from '../WechatConect/WechatKit'
 import css from './recharge.less'
 import { Link } from 'react-router'
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Row, Col, Button, message } from 'antd'
 
 const FormItem = Form.Item
 
-function onBridgeReady(){
-	var url = "http://wechat-api.tallty.com/cloud_closet_wechat/wx_pay"
-  SuperAgent
-  	.post(url)
-    .set('Accept', 'application/json')
-    .send( {'openid': 'olclvwCOMobnRYQRtXLAdhujZbtM', 'total_fee': 1 } )
-    .end( (err, res) => {
-      if (res.ok) {
-      console.log(WeixinJSBridge);
-		   	WeixinJSBridge.invoke(
-	       	'getBrandWCPayRequest', {
-	          "appId": 'wx47b02e6b45bf1dad',     //公众号名称，由商户传入     
-	          "timestamp": res.body.timeStamp,         //时间戳，自1970年以来的秒数     
-	          "noncestr": res.body.nonceStr.xml.nonce_str, //随机串     
-	          "package": 'prepay_id='+res.body.nonceStr.xml.prepay_id,     
-	          "signType": res.body.signType,         //微信签名方式：     
-	          "paySign": res.body.paySign //微信签名
-	       	},
-	       	function(res){
-	       		console.log(res); 
-	          if(res.err_msg == "get_brand_wcpay_request：ok" ) {
-
-	          }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-	       	}
-		   	);
-	   	}
-  })  
+export class Recharge extends Component {
+	state = {
+		money: null
 	}
 
-export class Recharge extends Component {
+	componentWillMount() {
+		// WechatKit.getConfig()
+	}
 
-	handleSubmit(e) {
-		e.preventDefault();
-    console.log('收到表单值：', this.props.form.getFieldsValue());
+	componentDidMount() {
+		WechatKit.getConfig()
+		// this.pay_sign()
+	}	
+
+	onBridgeReady(){
+	  SuperAgent
+	  	.post("http://wechat-api.tallty.com/cloud_closet_wechat/wx_pay")
+	    .set('Accept', 'application/json')
+	    .send( {'openid': 'olclvwCOMobnRYQRtXLAdhujZbtM', 'total_fee': 1 } )
+	    .end( (err, res) => {
+	      if (res.ok) {
+	      console.log(WeixinJSBridge);
+			   	WeixinJSBridge.invoke(
+		       	'getBrandWCPayRequest', {
+		          "appId": 'wx47b02e6b45bf1dad',     //公众号名称，由商户传入     
+		          "timestamp": res.body.timeStamp,         //时间戳，自1970年以来的秒数     
+		          "noncestr": res.body.nonceStr.xml.nonce_str, //随机串     
+		          "package": 'prepay_id='+res.body.nonceStr.xml.prepay_id,     
+		          "signType": res.body.signType,         //微信签名方式：     
+		          "paySign": res.body.paySign //微信签名
+		       	},
+		       	function(res){
+		       		console.log(res); 
+		          if(res.err_msg == "get_brand_wcpay_request：ok" ) {
+
+		          }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+		       	}
+			   	);
+		   	}
+	  })  
+	}
+
+	/**
+	 * 充值面额
+	 */
+	getMoneyCard() {
+		let cards = [500, 1000, 2000, 3000, 5000, 10000]
+		let list = []
+		cards.forEach((item, i, obj) => {
+			list.push(
+				<Col span={8} className={css.col} key={item}>
+					<Button className={css.card} 
+									onClick={this.handleChooseMoney.bind(this, item)}>{item}元</Button>
+				</Col>
+			)
+		})
+		return list
+	}
+
+	/**
+	 * [handleInputChange 表单输入事件]
+	 * @param  {[type]} e [表单]
+	 */
+	handleInputChange(e) {
+		this.setState({ money: e.target.value })
+	}
+
+	/**
+	 * 选择充值面额点击事件
+	 */
+	handleChooseMoney(item) {
+		console.log("你选择了"+item+"元");
+		this.setState({ money: item })
+	}
+
+	/**
+	 * [handlePay 开始微信支付]
+	 */
+	handlePay() {
+		if (this.state.money > 0) {
+			console.log("=======开始支付=======")
+
+		} else {
+			message.error("请选择充值金额", 2)
+		}
 	}
 
 	pay_sign(){
@@ -59,46 +110,42 @@ export class Recharge extends Component {
 		}
 	}
 
-	componentWillMount() {
-		// WechatKit.getConfig()
-	}
-
-	componentDidMount() {
-		WechatKit.getConfig()
-		// this.pay_sign()
-	}
-
 	render() {
-		const { getFieldProps } = this.props.form
+		const { getFieldDecorator } = this.props.form
+		const { money } = this.state
+
 		return (
 			<div className={css.container}>
-				<div className={css.item}>
-					<div className={css.label}>储蓄卡</div>
-					<div className={css.item_right}>
-						<p className={css.card}>交通银行（1011）</p>
-						<p className={css.tips}>单日交易限额 ¥ 50000.00</p>
-					</div>
-				</div>
 
 				<Form inline onSubmit={this.handleSubmit}>
-					<div className={css.item}>
-						<div className={css.label} style={{lineHeight: '30px'}}>金额(元)</div>
-						<div className={css.item_right}>
-							<FormItem>
-								<Input placeholder="请输入金额" {...getFieldProps('money')} className={css.input} type="number" />
-							</FormItem>
-						</div>
-					</div>
-
-					{/*
-						<FormItem>
-						<Button type="primary" htmlType="submit" className={css.next}>确定</Button>
+					<FormItem>
+						<Input placeholder="充值金额" {...getFieldDecorator('money')} 
+									 className={css.input} 
+									 onChange={this.handleInputChange.bind(this)}
+									 type="number"
+									 value={money} />
 					</FormItem>
-					<Button type="primary" className={css.next} onClick={WechatKit.chooseWXPay()}>确定</Button>
-					*/}
-					<Button type="primary" className={css.next} onClick={this.pay_sign}>确定</Button>
-					<Link to="/user" className={css.next}>下一步</Link>
 				</Form>
+
+				{/* 充值面额 */}
+				<div className={css.kinds}>
+					<Row className={css.row}>
+						{ this.getMoneyCard() }
+					</Row>
+				</div>
+
+				{/* 操作 */}
+				<div className={css.actions}>
+					<Button className={css.pay_btn} onClick={this.handlePay.bind(this)}>
+						<img src="src/images/wechat_pay.svg" alt=""/>微信支付
+					</Button>
+					<Button className={css.other_pay_btn}>找人代付（POS机刷卡）</Button>
+					<p className={css.search}>
+						<Link to="/user">查余额</Link>
+						<span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;</span>
+						<Link to="/user">我的充值记录</Link>
+					</p>
+				</div>
 			</div>
 		)
 	}
