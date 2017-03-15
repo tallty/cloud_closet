b51daf819c4ecb1ce45194669261d9f01e0ec9a6
iboying <weboying@gmail.com>
2017-03-15 19:16:46 +0800

    Fix vip page issue.

--- src/components/user/Notification.jsx
+++ src/components/user/Notification.jsx
@@ -16,9 +16,9 @@ export class Notification extends Component {
 
   componentDidMount() {
     let cells = [
-      {id: 1, url: "/src/images/notification_icon0.png", name: "物流更新", content: "物流更新", time: "8:30"},
-      {id: 2, url: "/src/images/notification_icon1.png", name: "通知", content: "通知", time: "两小时前"},
-      {id: 3, url: "/src/images/notification_icon2.png", name: "活动通知", content: "活动通知", time: "一天前"}
+      { id: 1, url: "/src/images/notification_icon0.png", name: "物流更新", content: "物流更新", time: "8:30" },
+      { id: 2, url: "/src/images/notification_icon1.png", name: "通知", content: "通知", time: "两小时前" },
+      { id: 3, url: "/src/images/notification_icon2.png", name: "活动通知", content: "活动通知", time: "一天前" }
     ]
     this.setState({ cells: cells })
   }
@@ -30,12 +30,12 @@ export class Notification extends Component {
         <Link to="/notifications" className={css.notification_cell} key={cell.id}>
           <Row>
             <Col span={19}>
-              <img src={ cell.url } alt="卡券图标"/>
-              <p>{ cell.name }</p>
-              <p className={css.content}>{ cell.content }</p>
+              <img src={cell.url} alt="卡券图标" />
+              <p>{cell.name}</p>
+              <p className={css.content}>{cell.content}</p>
             </Col>
             <Col span={5}>
-              <span className={css.time}>{ cell.time }</span>
+              <span className={css.time}>{cell.time}</span>
             </Col>
           </Row>
         </Link>
@@ -43,14 +43,14 @@ export class Notification extends Component {
     })
     return list
   }
-  
+
   render() {
     return (
       <div className={css.container}>
         <Toolbar title="系统通知" url="/user" />
         <div className={css.content}>
           <div style={{ height: 10 }}></div>
-          { this.state.cells ? this.cells() : <Spiner/> }
+          {this.state.cells ? this.cells() : <Spiner />}
         </div>
       </div>
     )
@@ -58,7 +58,7 @@ export class Notification extends Component {
 }
 
 Notification.defaultProps = {
-  
+
 }
 
 Notification.propTypes = {
