b661f276afe5d9e64f0f3e527dd586e0be71da8c
MC <mc@ACA80108.ipt.aol.com>
2017-03-17 21:44:20 +0800

    edit "components/user/receipt/ReceiptRecord"

--- src/components/user/receipt/ReceiptRecord.jsx
+++ src/components/user/receipt/ReceiptRecord.jsx
@@ -19,13 +19,16 @@ export class ReceiptRecord extends Component {
     let type = this.state.type
     let balance = this.state.balance
     return (
-      <div>
-        <Toolbar url="/#" title="开票记录" theme="dark"></Toolbar>
+      <div className={css.container}>
+        <Toolbar url="/#" title="开票记录" theme="dark" className={css.tops} />
         <div className={css.content}>
-          <div className={css.money}>{money}元</div>
-          <div className={css.type}>{type}</div>
-          <div className={css.balance}>开票额度剩余{balance}元</div>
-          <div className={css.date}>开票日期：{date}</div>
+          <div className={css.content}>
+            <div className={css.money}>{money}元</div>
+            <div className={css.types}>{type}</div>
+            <div className={css.balances}>开票额度剩余{balance}元</div>
+            <div className={css.dates}>开票日期：{date}</div>
+            <img src="src/images/receipt_success.png" alt="" className={css.img_style}/>
+          </div>
         </div>
       </div>
     );
