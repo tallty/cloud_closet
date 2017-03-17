6944332764d682b0d45b3e8a7da05b8256dcc3f8
MC <mc@ACA80104.ipt.aol.com>
2017-03-17 09:41:48 +0800

    edit "components/user/Receipt.jsx"

--- src/components/user/Receipt.jsx
+++ src/components/user/Receipt.jsx
@@ -1,10 +1,10 @@
 /**
  * 个人中心 - 发票
  */
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import css from './receipt.less'
 import Toolbar from '../common/Toolbar';
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 
 export class Receipt extends Component {
   state = {
@@ -12,8 +12,12 @@ export class Receipt extends Component {
   }
 
   render() {
-    let tips = this.state.money ? '可开发票额度' : '（暂不可开，单张发票需大于50元）';
-    let money = this.state.money ? `${this.state.money}元` : '--';
+    let tips = this.state.money
+      ? '可开发票额度'
+      : '（暂不可开，单张发票需大于50元）';
+    let money = this.state.money
+      ? `${this.state.money}元`
+      : '--';
     return (
       <div className={css.container}>
         <Toolbar url="/user" title="发票" theme="dark">
@@ -26,10 +30,8 @@ export class Receipt extends Component {
             <button>我要开票</button>
             <ul>
               <li>最高额度仅限实际充值或信用卡的订单费用，不包含任何优惠券、赠送金额、体验券等优惠活动费用</li>
-              <li>您可开1张或多张，单张发票不得低于50元</li>
-              <li>非钻石卡用户，10个工作日内处理（不含邮寄时间）</li>
-              <li>钻石卡用户，3个工作日内处理（不含邮寄时间）</li>
-              <li>开票金额大于或等于1000元可为您免费邮寄</li>
+              <li>您可开1张或多张，单张发票不得低于1000元</li>
+              <li>开票在5个工作日内处理（不含邮寄时间）</li>
             </ul>
           </div>
         </div>
@@ -38,9 +40,5 @@ export class Receipt extends Component {
   }
 }
 
-Receipt.defaultProps = {
-
-}
-Receipt.propTypes = {
-
-}
+Receipt.defaultProps = {}
+Receipt.propTypes = {}
