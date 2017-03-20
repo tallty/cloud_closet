5aed50357b12e14c7ef7b5c8f4eb7920a1a64f95
MC <mc@ACA80106.ipt.aol.com>
2017-03-20 09:53:24 +0800

    edit "receipt/ReceiptRecord#background"

--- src/components/user/receipt/ReceiptRecord.jsx
+++ src/components/user/receipt/ReceiptRecord.jsx
@@ -5,7 +5,7 @@ import React, { Component, PropTypes } from 'react';
 import SuperAgent from 'superagent';
 import css from './ReceiptRecord.less';
 import Toolbar from '../../common/ToolBar';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 import { Spiner } from '../../common/Spiner';
 import { Row, Col } from 'antd';
 
@@ -18,17 +18,17 @@ export class ReceiptRecord extends Component {
       date: '2017-03-15',
       type: '普通发票',
       balance: '300'
-    },{
+    }, {
       money: '200',
       date: '2017-03-15',
       type: '增值税专用发票',
       balance: '300'
-    },{
+    }, {
       money: '200',
       date: '2017-03-15',
       type: '普通发票',
       balance: '300'
-    },{
+    }, {
       money: '200',
       date: '2017-03-15',
       type: '增值税专用发票',
@@ -38,7 +38,7 @@ export class ReceiptRecord extends Component {
 
   //开票记录列表
   getReceipts(page) {
-    SuperAgent 
+    SuperAgent
       .get('http://closet-api.tallty.com/receipts?page=${page}')
       .set('Accept', 'application/json')
       .set('X-User-Token', localStorage.authentication_token)
@@ -46,9 +46,9 @@ export class ReceiptRecord extends Component {
       .end((err, resualt) => {
         if (resualt.ok) {
           const obj = resualt.body;
-          this.setState({receipts: obj.receipts.reverse() });
+          this.setState({ receipts: obj.receipts.reverse() });
         } else {
-  
+
         }
       })
   }
@@ -61,8 +61,8 @@ export class ReceiptRecord extends Component {
       list.push(
         <div className={css.content_detail} key={index}>
           <Row className={css.row_top}>
-              <Col span={12} className={css.money}>{receipt.money}元</Col>
-              <Col span={12} className={css.types}>{receipt.type}</Col>
+            <Col span={12} className={css.money}>{receipt.money}元</Col>
+            <Col span={12} className={css.types}>{receipt.type}</Col>
           </Row>
           <Row className={css.row_bottom}>
             <Col span={14}>
@@ -70,12 +70,12 @@ export class ReceiptRecord extends Component {
               </p>
               <p className={css.dates}>开票日期：{receipt.date}</p>
             </Col>
-            <Col span={10}><img src="src/images/receipt_success.png" alt="" className={css.img_style}/></Col>
+            <Col span={10}><img src="src/images/receipt_success.svg" alt="" className={css.img_style} /></Col>
           </Row>
         </div>
       )
     })
-    return list 
+    return list
   }
 
   // 空列表样式
@@ -93,7 +93,7 @@ export class ReceiptRecord extends Component {
       <div className={css.container}>
         <Toolbar url="/user" title="开票记录" theme="dark" className={css.tops} />
         <div className={css.content}>
-          { Receipts.length > 0 ? Receipts : this.getReceiptsNone()}
+          {Receipts.length > 0 ? Receipts : this.getReceiptsNone()}
         </div>
       </div>
     );
