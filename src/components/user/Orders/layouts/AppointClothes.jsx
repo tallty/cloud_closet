b4d0bad4050db5306f385d95f9329f6ffc84e3c5
iboying <weboying@gmail.com>
2017-05-23 12:09:55 +0800

    Imporve appointment number

--- src/components/user/Orders/layouts/AppointClothes.jsx
+++ src/components/user/Orders/layouts/AppointClothes.jsx
@@ -6,6 +6,20 @@ const { string, number, arrayOf, shape } = PropTypes;
 
 export class AppointClothes extends Component {
 
+  // 数量范围描述数据存到数据库时，可删除此方法
+  showAppointmentNumber(num) {
+    switch (num) {
+      case 10:
+        return '5-20';
+      case 30:
+        return '20-50';
+      case 50:
+        return '大于50';
+      default:
+        return num;
+    }
+  }
+
   render() {
     const { order } = this.props;
     return (
@@ -15,7 +29,7 @@ export class AppointClothes extends Component {
         </div>
         <div className={css.right}>
           <div className={css.content}>
-            <p>衣橱预约：{order.number}件</p>
+            <p>衣橱预约：{this.showAppointmentNumber(order.number)}件</p>
           </div>
         </div>
       </div>
